71b6ddf1e1452e3045e1a9b4b649c0d8287cfc02
madhusmita <madhusmitap@mindfiresolutions.com>
2014-01-14 14:40:16 +0530

    Moved code for send custom extension request to after successful logged-in
    Signed-off-by: madhusmita <madhusmitap@mindfiresolutions.com>

--- SFSJavaClient/src/com/sfs/test/MyTest.java
+++ SFSJavaClient/src/com/sfs/test/MyTest.java
@@ -12,7 +12,6 @@ import sfs2x.client.requests.ExtensionRequest;
 import sfs2x.client.requests.JoinRoomRequest;
 import sfs2x.client.requests.LoginRequest;
 
-import java.awt.Event;
 import java.util.Map;
 
 public class MyTest
@@ -22,7 +21,7 @@ public class MyTest
     public static void main(String args[]) {
     	
     	sfs = new SmartFox(true);
-        /*
+    	
         sfs.addEventListener(SFSEvent.EXTENSION_RESPONSE, new IEventListener() {
 			
 			@Override
@@ -36,31 +35,21 @@ public class MyTest
 					 System.out.println("The sum is: " + responseParams.getInt("sum"));
 				}
 			}
-		});*/    	
-    	
+		});
+        
         // Add event handler for connection
         sfs.addEventListener(SFSEvent.CONNECTION, new IEventListener()
         {
             public void dispatch(BaseEvent evt) throws SFSException {
                 
                 // Retrieve event parameters
-                Map params = evt.getArguments();
+                Map<String, Object> params = evt.getArguments();
                 
                 if ((Boolean)params.get("success")) {
                     System.out.println("Connection established");
-    				LoginRequest request = new LoginRequest("madhusmita1", "mindfire", "TestZone");
+    				LoginRequest request = new LoginRequest("madhusmita", "mindfire", "TestZone");
     				System.out.println("Logging in");
     				sfs.send(request);
-    				
-    				sfs.send(new JoinRoomRequest("MyLobby"));
-    				/*
-    		        // Send two integers to the Zone extension and get their sum in return
-    		        ISFSObject params1 = new SFSObject();
-    		        params1.putInt("n1", 26);
-    		        params1.putInt("n2", 16);
-    		        
-    		        sfs.send(new ExtensionRequest("math", params1));
-    		        */
                 }
                 else
                     System.out.println("Connection failed");
@@ -71,9 +60,18 @@ public class MyTest
         sfs.addEventListener(SFSEvent.LOGIN, new IEventListener() {
 			
 			@Override
-			public void dispatch(BaseEvent arg0) throws SFSException {
+			public void dispatch(BaseEvent evt) throws SFSException {
 				//LoginRequest request = new LoginRequest("madhusmita", "", "TestZone");
-				System.out.println("Login Success :)");
+				System.out.println("Login Success :)");				
+				
+				sfs.send(new JoinRoomRequest("MyLobby"));
+				
+		        // Send two integers to the Zone extension and get their sum in return
+		        ISFSObject params1 = new SFSObject();
+		        params1.putInt("n1", 26);
+		        params1.putInt("n2", 16);
+		        
+		        sfs.send(new ExtensionRequest("math", params1));			
 			}
 		});
         sfs.addEventListener(SFSEvent.LOGIN_ERROR, new IEventListener() {
