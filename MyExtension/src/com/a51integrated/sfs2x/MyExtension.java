fed79dd34d15613c0f816cf7ce9a922496489b3d
madhusmita <madhusmitap@mindfiresolutions.com>
2014-01-16 17:13:37 +0530

    Create room at server side while custom Java extension initialize

--- MyExtension/src/com/a51integrated/sfs2x/MyExtension.java
+++ MyExtension/src/com/a51integrated/sfs2x/MyExtension.java
@@ -1,13 +1,29 @@
 package com.a51integrated.sfs2x;
 
+import com.smartfoxserver.v2.api.CreateRoomSettings;
 import com.smartfoxserver.v2.core.SFSEventType;
+import com.smartfoxserver.v2.exceptions.SFSCreateRoomException;
 import com.smartfoxserver.v2.extensions.SFSExtension;
 
 public class MyExtension extends SFSExtension {
 
 	@Override
-	public void init() {
-		trace("Login extension starting.");
+	public void init() {		
+		try {
+			trace("Start creating dynamic room");
+			// Create new dynamic room
+	        CreateRoomSettings roomSetting = new CreateRoomSettings();
+	        roomSetting.setGroupId("Group 2");
+	        roomSetting.setName("Room2Name");
+	        roomSetting.setPassword("mindfire");
+	        getParentZone().createRoom(roomSetting);
+	        trace("End creating dynamic room");
+			//getApi().createRoom(getParentZone(), roomSetting, null);
+		} catch (SFSCreateRoomException e) {
+			e.printStackTrace();
+		}
+		
+		trace("Starting event handler starting.");
 		
 		this.addEventHandler(SFSEventType.USER_LOGIN, LoginHandler.class);
 		
